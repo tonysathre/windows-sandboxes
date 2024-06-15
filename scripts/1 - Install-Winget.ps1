e11785cba9e12185f0bda266fec06a7e0d0ed1b2
Tony Sathre <tony@ghostbit.org>
2024-06-15 14:46:45 -0500

    fix capitilization

--- scripts/1 - Install-Winget.ps1
+++ scripts/1 - Install-Winget.ps1
@@ -1,4 +1,4 @@
-$progressPreference = 'silentlyContinue'
+$ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
 Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile Microsoft.UI.Xaml.2.8.x64.appx
